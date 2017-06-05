d21bb3d0f79c6141b744c733cc0f576384bcdf58
hanglekhanh <hanglekhanh169@gmail.com>
2017-06-05 23:41:09 +0700

    signupwithAWS

--- src/Routes.js
+++ src/Routes.js
@@ -4,10 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './containers/Login';
 import Home from './containers/Home';
 import AppliedRoute from './components/AppliedRoute';
+import Signup from './containers/Signup';
+
 export default ({ childProps }) => (
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps}/>
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
+        <AppliedRoute path="/signup" exact component={Signup} props={childProps}/>
         <Route component={NotFound} />
     </Switch>
 );
