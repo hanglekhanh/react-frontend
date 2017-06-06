5e47a61bf5ab3af13f75d5629f660f8757bca64e
hanglekhanh <hanglekhanh169@gmail.com>
2017-06-07 00:09:23 +0700

    create Note page

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,13 @@ import Login from './containers/Login';
 import Home from './containers/Home';
 import AppliedRoute from './components/AppliedRoute';
 import Signup from './containers/Signup';
-
+import NewNote from './containers/NewNote';
 export default ({ childProps }) => (
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps}/>
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
         <AppliedRoute path="/signup" exact component={Signup} props={childProps}/>
+        <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps}/>
         <Route component={NotFound} />
     </Switch>
 );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     cognito: {
         USER_POOL_ID : 'us-east-2_woBZwJtbc',
         APP_CLIENT_ID : 'er3rctog424s05j9te4agqt5a',
